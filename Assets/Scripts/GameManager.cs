066f726ebeb9dce6fbda3128a1938546dbd93586
Thao <thaodvhe173219@fpt.edu.vn>
2025-02-28 17:13:18 +0700

    update fruit 2

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,14 +1,33 @@
-using System.Collections;
-using System.Collections.Generic;
+
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
     public static GameManager instance;
+    
     public Player player;
+
+    [Header("Fruits Management")]
+    public bool fruitsHaveRandomLook;
+
+    public int fruitsCollected;
     private void Awake()
     {
-        instance = this;
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+            
     }
-   
+    public void AddFruit()
+    {
+        fruitsCollected++;
+    }
+    public bool FruitsHaveRandomLook() => fruitsHaveRandomLook;
 }
+
+
